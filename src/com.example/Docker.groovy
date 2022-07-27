3d0c8737031d4a3e7b934dc5b85f133cef65ab36
umagloire <umagloire99@gmail.com>
2022-07-27 10:07:54 +0100

    oop with src folder
    Signed-off-by: umagloire <umagloire99@gmail.com>

---
+++ src/com.example/Docker.groovy
@@ -0,0 +1,22 @@
+package com.example
+
+/* groovylint-disable-next-line ClassJavadoc, CompileStatic, EmptyClass */
+class Docker implements Serializable {
+
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildImage(String imageName) {
+        script.echo 'building docker Image and push to docker hub ......'
+    /* groovylint-disable-next-line LineLength */
+       script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh 'echo $script.PASS | docker login -u $script.USER --password-stdin'
+            script.sh "docker push $imageName"
+        }
+    }
+
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,7 @@
 #!/usr/bin/env groovy
+import com.example.Docker
 
 /* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
 def call(String dockerImage) {
-    echo 'building docker Image and push to docker hub ......'
-    /* groovylint-disable-next-line LineLength */
-    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $dockerImage ."
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push $dockerImage"
-    }
+    return new Docker(this).buildImage(dockerImage)
 }
